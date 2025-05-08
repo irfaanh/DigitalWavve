0a6e9b07e36de1cb7320c22ac7b896bf9866c89c
irfaanh <muhammedirfan786678@gmail.com>
2025-05-08 11:14:00 +0530

    updated worksection

--- Client/src/Pages/OurWorks.jsx
+++ Client/src/Pages/OurWorks.jsx
@@ -3,21 +3,23 @@ import './OurWorks.css'
 
 const OurWorks = () => {
   const brandLogo = [
+
+
     { no: '1', name: 'Solaire', image: '/images/works/001.webp' },
     { no: '2', name: 'Hun Must', image: '/images/works/350430416_2459686380858175_3626943180530928288_n-1.webp' },
     { no: '3', name: 'QTile', image: '/images/works/391631850_710711944415257_4405738463106622495_n.webp' },
-    { no: '4', name: 'Lagnuvo', image: '/images/works/430050566_1393812864593801_3386726375752106791_n.jpg' },
-    { no: '5', name: 'Aroosa', image: '/images/works/Aroosa-1000x-662.webp' },
+    { no: '4', name: 'MG', image: '/images/works/WhatsApp-Image-2024-04-19-at-5.53-1-4.webp' },
+    { no: '5', name: 'Fundfloat', image: '/images/works/WhatsApp-Image-2024-04-19-at-5.53-1-7.png' },
     { no: '6', name: 'Akriti', image: '/images/works/Artboard-1-1.webp' },
-    { no: '7', name: 'Aroosa', image: '/images/works/DSC00086-1-1.webp' },
-    { no: '8', name: 'Muthoot', image: '/images/works/IMG-20240318-WA0134.webp' },
-    { no: '9', name: 'Ovolo', image: '/images/works/ovolo-X-QC-02.webp' },
-    { no: '10', name: 'Glamata', image: '/images/works/psd-6-scaled.webp' },
-    { no: '11', name: 'Gratis', image: '/images/works/QCXgratis-cover.webp' },
-    { no: '12', name: 'Impex', image: '/images/works/Vadamvali-100x-662.webp' },
-    { no: '13', name: 'Wea Design', image: '/images/works/wea-design-5-1.webp' },
-    { no: '14', name: 'MG', image: '/images/works/WhatsApp-Image-2024-04-19-at-5.53-1-4.webp' },
-    { no: '15', name: 'Fundfloat', image: '/images/works/WhatsApp-Image-2024-04-19-at-5.53-1-7.png' }
+    { no: '7', name: 'Lagnuvo', image: '/images/works/430050566_1393812864593801_3386726375752106791_n.jpg' },
+    { no: '8', name: 'Aroosa', image: '/images/works/Aroosa-1000x-662.webp' },
+    { no: '9', name: 'Aroosa', image: '/images/works/DSC00086-1-1.webp' },
+    { no: '10', name: 'Muthoot', image: '/images/works/IMG-20240318-WA0134.webp' },
+    { no: '11', name: 'Ovolo', image: '/images/works/ovolo-X-QC-02.webp' },
+    { no: '12', name: 'Glamata', image: '/images/works/psd-6-scaled.webp' },
+    { no: '13', name: 'Gratis', image: '/images/works/QCXgratis-cover.webp' },
+    { no: '14', name: 'Impex', image: '/images/works/Vadamvali-100x-662.webp' },
+    { no: '14', name: 'Wea Design', image: '/images/works/wea-design-5-1.webp' },
   ];
 
   return (
